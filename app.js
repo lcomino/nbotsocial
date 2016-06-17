3fcdc757ccdefebb9c350aa0696c6b967873f6ac
lcomino <lcomino3d@gmail.com>
2016-06-16 22:46:41 -0300

    Adicionado bower ao projeto, e o momentjs para manipulacao de data, mas apanhando... :(

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -7,6 +9,8 @@ var bodyParser = require('body-parser');
 var load = require('express-load');
 var session = require('express-session');
 var mongoose = require('mongoose');
+
+
 //var autenticator = require('./middleware/autenticator');
 
 //load('middleware', {verbose : true});
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,6 @@
+var moment = require('moment');
+moment.locale('pt-br');
+
 module.exports = function(app){
 
     var Post = app.models.posts;
@@ -5,20 +8,31 @@ module.exports = function(app){
     var PostsController = {
         index : function(req, res){
             var params = {};
-            Post.find({}, function(err, posts){
-                params.posts = posts;
-                console.log(posts);
+            Post.find({}, function(err, p){
+                
+
+                params.posts = p.map(function(post){
+                    var postNovo = post;
+                    postNovo.programadoPara = post.programadoPara.map(function(data){                        
+                        return moment(new Date(data)).format('DD/MM/YYYY HH:mm:ss');
+                    });
+
+                    console.log(postNovo);
+                    return postNovo;                    
+                });
+                
                 res.render('posts/index', params);
             });            
         },
-        novo : function(req, res){
+        novo : function(req, res){            
             res.render('posts/novo');
         },
         cadastrar : function(req, res){            
-            console.log(req.body);
+                        
             var user = {
                 conteudo : req.body.conteudo,
-                redeSocial : req.body.redeSocial
+                redeSocial : req.body.redeSocial,
+                programadoPara : moment(req.body.programadoParaData + ' ' + req.body.programadoParaHora).toDate()
             };
 
             Post.create(user, function(err, usuario){
